e8537887863c3b8cbd2eb1ca147fc9ff9cc8cae7
Casey Devet <casey.devet@sccdsb.net>
2020-09-22 12:49:57 -0400

    Create AboutMe.java

---
+++ AboutMe.java
@@ -0,0 +1,24 @@
+/*
+ * Programmer:
+ * Date:
+ * Purpose:
+ */
+
+public class AboutMe {
+    public static void main (String [] args) {
+        // Declaring the variables
+        // TODO: Write the code the declares the variables
+
+        // Print the values
+        System.out.println("Hi, my name is " + myName);
+        System.out.println("I am " + myAge + " years old.");
+        System.out.println("My height is " + myHeightInMetres + " m.");
+
+        // Change the variables
+        // TODO: Write the code that changes the variables
+
+        // Print the new values
+        System.out.println("Some time passes...");
+        // TODO: Write the last two print statements
+    }
+}
