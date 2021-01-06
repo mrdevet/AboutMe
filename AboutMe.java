0fcbc549c92d41528282a4a3b1cdf7248f77900d
Casey Devet <casey.devet@sccdsb.net>
2021-01-06 00:54:50 -0500

    Fixed TODO comments.

--- AboutMe.java
+++ AboutMe.java
@@ -6,16 +6,16 @@
 
 public class AboutMe {
     public static void main (String [] args) {
-        // Declaring the variables
-        // TODO: Write the code the declares the variables
+        // TODO: Declare the variables
+        
 
         // Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
-        // Change the variables
-        // TODO: Write the code that changes the variables
+        // TODO: Change the variables
+        
 
         // Print the new values
         System.out.println("Some time passes...");
